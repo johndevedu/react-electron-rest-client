a029893da84c2be4d919a8de8b625150f8e2a205
John Lee <john.lee@sabio.la>
2018-07-08 16:51:46 -0700

    request POC

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,16 @@
 /* eslint flowtype-errors/show-errors: 0 */
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import React from "react";
+import { Switch, Route } from "react-router";
+import App from "./containers/App";
+import HomePage from "./containers/HomePage";
+import CounterPage from "./containers/CounterPage";
+import RequestPage from "./containers/RequestPage";
 
 export default () => (
   <App>
     <Switch>
       <Route path="/counter" component={CounterPage} />
+      <Route path="/request" component={RequestPage} />
       <Route path="/" component={HomePage} />
     </Switch>
   </App>
